1abf62b92d4e14614e9cf53c8cdd96bfc9d57b35
Samuel Santos Gomes de Souza <Samuels.g.desouza@gmail.com>
2024-02-22 09:03:43 -0300

    att

--- teste.py
+++
@@ -1,8 +0,0 @@
-from licenses import License
-
-licenses = License(path_folder="licenses")
-
-license = licenses.delete_license(license_key="4312412341")
-license = licenses.check_license(license_key="4312412341")
-print(license) # False
-
